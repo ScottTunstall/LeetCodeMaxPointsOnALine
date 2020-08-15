1467589397da036456bec0d46dbf9c98a2d40c97
Scott Tunstall <Scott.tunstall@ntlworld.com>
2020-08-15 12:53:32 +0100

    Without cheating, I refactored the code to take slope into account.  This is a wee bit more promising, but dupe points need to be addressed.

--- LeetCodeMaxPointsOnALine/Program.cs
+++ LeetCodeMaxPointsOnALine/Program.cs
@@ -8,126 +8,25 @@ namespace LeetCodeMaxPointsOnALine
 {
     class Program
     {
-        private static Dictionary<string, int> _screenMap = new Dictionary<string, int>();
-
-        private struct Rect
-        {
-            public int X1;
-            public int Y1;
-            public int X2;
-            public int Y2;
-
-            public bool IsInBounds(in int x, in int y)
-            {
-                return (x >= X1 && x <= X2 && y >= Y1 && y <= Y2);
-            }
-        }
-
-        public static int MaxPoints(int[][] points)
-        {
-            if (points.Length == 0)
-                return 0;
-
-            var asList = new List<Point>();
-
-            foreach (var arrItem in points)
-            {
-                asList.Add(new Point(arrItem[0], arrItem[1]));
-            }
-
-            var minX = asList.Min(x => x.X);
-            var maxX = asList.Max(x => x.X);
-            var minY = asList.Min(x => x.Y);
-            var maxY = asList.Max(x => x.Y);
-            var rect = new Rect() {X1 = minX, X2 = maxX, Y1 = minY, Y2 = maxY};
-        
-            foreach (var pt in asList)
-            {
-                var key = pt.X + "_" + pt.Y;
-                if (!_screenMap.ContainsKey(key))
-                    _screenMap[key] = 1;
-                else
-                    _screenMap[key] = _screenMap[key] + 1;
-            }
-
-            int best = 0;
-            foreach (var pt in asList)
-            {
-                var key = pt.X + "_" + pt.Y;
-                int count = 0;
-                int toAdd = _screenMap[key];
-                
-                // Left
-                count = CountPoints(pt, -1, 0, rect) + toAdd;
-                best = Math.Max(best, count) ;
-
-                // Above Left
-                count = CountPoints(pt, -1, 1,rect) + toAdd;
-                best = Math.Max(best, count) ;
-                
-                // Above
-                count = CountPoints(pt, 0,1,rect) + toAdd;
-                best = Math.Max(best, count) ;
-
-                // Above Right
-                count = CountPoints(pt, 1, 1, rect) + toAdd;
-                best = Math.Max(best, count);
-
-                // Right
-                count = CountPoints(pt, 1, 0, rect) + toAdd;
-                best = Math.Max(best, count);
-
-                // Below Right
-                count = CountPoints(pt, 1, -1, rect) + toAdd;
-                best = Math.Max(best, count);
-
-                // Below 
-                count = CountPoints(pt, 0, -1, rect) + toAdd;
-                best = Math.Max(best, count);
-
-                // Below left
-                count = CountPoints(pt, -1, -1, rect) + toAdd;
-                best = Math.Max(best, count);
-            }
-
-
-            return best;
-        }
-
-
-        private static int CountPoints(Point startPoint, in int deltaX, in int deltaY, Rect rect)
-        {
-            int x = startPoint.X+deltaX;
-            int y = startPoint.Y+deltaY;
-
-            int count = 0;
-            while (rect.IsInBounds(x,y))
-            {
-                if (HasPoint(x,y))
-                    count++;
-
-                x += deltaX;
-                y += deltaY;
-            }
-
-            return count;
-        }
-
-
-        private static bool HasPoint(int x, int y)
-        {
-            var key = x + "_" + y;
-            if (_screenMap.TryGetValue(key, out int value))
-            {
-                return value == 1;
-            }
-
-            return false;
-        }
-
 
         static void Main(string[] args)
         {
+
+            int[][] points;
+            int count;
+            var sol = new Solution();
+
+            points = new int[][]
+            {
+                new[] {1, 1},
+                new[] {1, 1},
+                new[] {1, 1}
+            };
+
+            count = sol.MaxPoints(points);
+            Debug.Assert(count == 3);
+
+
             //var points = new int[][]
             //{
             //    new[] {1, 1},
@@ -135,25 +34,31 @@ namespace LeetCodeMaxPointsOnALine
             //    new[] {3,3}
             //};
 
-            //Debug.Assert(MaxPoints(points) == 3);
+            //var count = sol.MaxPoints(points);
+            //Debug.Assert(count == 3);
 
-            var points = new int[][]
-            {
-                new[] {1, 1},
-                new[] {3, 2},
-                new[] {5,3},
-                new[] {4,1},
-                new[] {2,3},
-                new[] {1,4},
-            };
-
-            //var points = new int[][]
+            //points = new int[][]
             //{
-            //    new[] {0, 0},
-            //    new[] {0, 0},
+            //    new[] {1, 1},
+            //    new[] {3, 2},
+            //    new[] {5,3},
+            //    new[] {4,1},
+            //    new[] {2,3},
+            //    new[] {1,4},
             //};
 
-            var count = MaxPoints(points);
+            //count = sol.MaxPoints(points);
+            //Debug.Assert(count==4);
+
+
+            points = new int[][]
+            {
+                new[] {0, 0},
+                new[] {0, 0},
+            };
+
+            count = sol.MaxPoints(points);
+            Debug.Assert(count == 2);
 
             Console.WriteLine(count);
         }
---
+++ LeetCodeMaxPointsOnALine/Solution.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace LeetCodeMaxPointsOnALine
+{
+    public class Solution
+    {
+        Dictionary<string, int> _slopeCountDictionary = new Dictionary<string, int>();
+
+        public int MaxPoints(int[][] points)
+        {
+            if (points.Length <2)
+                return points.Length;
+                
+            _slopeCountDictionary.Clear();
+            var asList = new List<Point>();
+
+            foreach (var arrItem in points)
+            {
+                asList.Add(new Point(arrItem[0], arrItem[1]));
+            }
+
+            for (int i = 0; i < asList.Count - 1; i++)
+            {
+                var startPoint = asList[i];
+                
+                for (int j = 1; j < asList.Count; j++)
+                {
+                    var endPoint = asList[j];
+
+                    var slope = CalculateSlope(startPoint, endPoint);
+
+                    string key = i + "_" + slope.ToString();
+                    IncrementValueForKey(key);
+                }
+            }
+
+            return _slopeCountDictionary.Max(x => x.Value) + 1;
+        }
+
+        private void IncrementValueForKey(in string key)
+        {
+            if (!_slopeCountDictionary.ContainsKey(key))
+            {
+                _slopeCountDictionary[key] = 1;
+            }
+            else
+            {
+                _slopeCountDictionary[key] = _slopeCountDictionary[key] + 1;
+            }
+        }
+
+        private static float CalculateSlope(Point p1, Point p2)
+        {
+            if (p1 == p2)
+                return 0;
+
+            int x1 = p1.X;
+            int y1 = p1.Y;
+            int x2 = p2.X;
+            int y2 = p2.Y;
+
+            float slope = (float)(y2 - y1) / (x2 - x1);
+            return (float)Math.Round(slope, 2);
+        }
+    }
+}
